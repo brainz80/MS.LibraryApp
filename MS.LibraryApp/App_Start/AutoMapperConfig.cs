ef6f92e9dc357135bd0031dcbceb8e51483aa59b
Mats Sibelius <sibelium@gmail.com>
2015-12-04 13:57:25 +0200

    Lisäsin AutoMapperConfig:in

---
+++ MS.LibraryApp/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MS.LibraryApp.Models;
+using MS.LibraryApp.Models.ViewModels;
+
+namespace MS.LibraryApp
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterAutoMapper()
+        {
+            var mapper = AutoMapper.Mapper.CreateMap<Book, BookViewModel>();
+            mapper.ReverseMap();
+        }
+    }
+}
--- MS.LibraryApp/Global.asax.cs
+++ MS.LibraryApp/Global.asax.cs
@@ -13,6 +13,7 @@ namespace MS.LibraryApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            AutoMapperConfig.RegisterAutoMapper();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
